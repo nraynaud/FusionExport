8df856296386cc719f5deb805bd1cb761dd5bd98
Nicolas Raynaud <github@nraynaud.com>
2017-10-26 17:35:00 -0700

    overhaul: now save in attributes, better UI

--- Export.py
+++ Export.py
@@ -69,117 +69,222 @@ def handle(handler, clazz):
     return handler_instance
 
 
+def get_export_list():
+    attributes = Application.get().activeProduct.findAttributes('nraynaud-Export', 'export')
+    result = []
+    for attr in attributes:
+        body = attr.parent
+        content = json.loads(attr.value)
+        if body:
+            for entry in content:
+                result.append({'body': body, 'file': entry['file']})
+    return result
+
+
 def create_setting_panel(args):
     app = Application.get()
     ui = app.userInterface
     design = app.activeProduct
     cmd = Command.cast(args.command)
 
-    def handle_file_input(args):
+    shadow_table = []
+    deletion_table = []
+    shadow_dict = {}
+    current_file = None
+
+    def select_row(new_row):
+        nonlocal current_file
+        set_detail_visibility(True)
+        body = new_row.get('body')
+        if body:
+            selection_input.addSelection(body)
+        file_input.text = str(new_row.get('file'))
+        current_file = new_row.get('file')
+
+    def save_if_you_can():
+        if selection_input.selectionCount and current_file is not None:
+            if table.selectedRow == -1:
+                row_index = table.rowCount
+                row_id = 'TableInput_string{}'.format(row_index)
+                body = selection_input.selection(0).entity
+                shadow_row = {'id': row_id, 'file': current_file, 'body': body, 'new': True}
+                shadow_table.append(shadow_row)
+                shadow_dict[row_id] = shadow_row
+                string_input = table.commandInputs.addStringValueInput(row_id, 'String', current_file)
+                string_input.isReadOnly = True
+                shadow_row['input'] = string_input
+                table.addCommandInput(string_input, row_index, 0)
+                table.selectedRow = row_index
+            else:
+                shadow_row = shadow_table[table.selectedRow]
+                # adding the old row to deletion list will remove the attribute from the element
+                deletion_table.append(shadow_row.copy())
+                shadow_row['file'] = current_file
+                shadow_row['body'] = selection_input.selection(0).entity
+
+    def set_detail_visibility(visible):
+        selection_input.clearSelection()
+        if visible:
+            selection_input.isVisible = True
+            file_input.isVisible = True
+        else:
+            selection_input.isVisible = False
+            file_input.isVisible = False
+
+    def handle_input_change(args):
+        nonlocal current_file
         event_args = InputChangedEventArgs.cast(args)
         cmd_input = event_args.input
+        if cmd_input.id == 'selection':
+            save_if_you_can()
         if cmd_input.id == 'selectFile':
             file_name = select_output_file(ui)
             if file_name:
+                current_file = file_name
                 cmd_input.text = file_name
-
-    cmd.inputChanged.add(handle(handle_file_input, InputChangedEventHandler))
+                save_if_you_can()
+        if cmd_input.id == 'addExport':
+            table.selectedRow = -1
+            remove_export.isEnabled = False
+            current_file = None
+            file_input.text = 'select file'
+            set_detail_visibility(True)
+        if cmd_input.id == 'removeExport':
+            shadow_row = shadow_table[table.selectedRow]
+            deletion_table.append(shadow_row)
+            del shadow_table[table.selectedRow]
+            del shadow_dict[shadow_row['id']]
+            table.deleteRow(table.selectedRow)
+            table.selectedRow = -1
+            remove_export.isEnabled = False
+            current_file = None
+            file_input.text = 'select file'
+            set_detail_visibility(False)
+        else:
+            shadow_row = shadow_dict.get(cmd_input.id)
+            if shadow_row:
+                select_row(shadow_row)
+                remove_export.isEnabled = True
 
     inputs = cmd.commandInputs
+    table = inputs.addTableCommandInput('sampleTable', 'Table', 2, '1:1')
+    add_export = inputs.addBoolValueInput('addExport', 'Add Export', False, '', True)
+    add_export.text = '+'
+    add_export.tooltip = 'Add an export'
+    table.addToolbarCommandInput(add_export)
+    remove_export = inputs.addBoolValueInput('removeExport', 'Remove Export', False, '', True)
+    remove_export.text = '-'
+    remove_export.tooltip = "Don't export this anymore"
+    remove_export.isEnabled = False
+    table.addToolbarCommandInput(remove_export)
     cmd.okButtonText = 'Ok Export'
     selection_input = inputs.addSelectionInput('selection', 'Body', 'Basic select command input')
-    selection_input.setSelectionLimits(1)
     selection_input.addSelectionFilter('Bodies')
+    # allow 0 selection so that it doesn't invalidate the entire command if empty
+    selection_input.setSelectionLimits(0, 1)
     file_input = inputs.addBoolValueInput('selectFile', 'File', False, '', True)
     file_input.text = 'select file'
+    set_detail_visibility(False)
 
     def on_activation(args):
-        dir_param = design.userParameters.itemByName(USER_PARAM)
-        if dir_param:
-            data = json.loads(dir_param.comment)
-            body = find_entity_from_path(data['chain'], design.rootComponent, ui)
-            if body:
-                selection_input.addSelection(body)
-            url, is_stale, error = Foundation.NSURL.URLByResolvingBookmarkData_options_relativeToURL_bookmarkDataIsStale_error_(
-                Foundation.NSData.alloc().initWithBase64EncodedString_options_(data['file'], 0),
-                Foundation.NSURLBookmarkResolutionWithSecurityScope,
-                None,
-                None,
-                None)
-            if not error:
-                file_path = url.path()
-                if file_path:
-                    file_input.text = file_path
+        exports = get_export_list()
+        index = 0
+        for export in exports:
+            file_path = decode_bookmark(export['file'])
+            row_id = 'TableInput_string{}'.format(index)
+            shadow_row = {'id': row_id, 'file': file_path, 'body': export['body']}
+            shadow_table.append(shadow_row)
+            shadow_dict[shadow_row['id']] = shadow_row
+            string_input = table.commandInputs.addStringValueInput(row_id, 'String', file_path)
+            shadow_row['input'] = string_input
+            string_input.isReadOnly = True
+            table.addCommandInput(string_input, index, 0)
+            index += 1
 
-    cmd.activate.add((handle(on_activation, CommandEventHandler)))
-
-    def get_bookmark_bytes(file_name):
-        url = Foundation.NSURL.alloc().initFileURLWithPath_(file_name)
-        bookmark, error = url.bookmarkDataWithOptions_includingResourceValuesForKeys_relativeToURL_error_(
-            Foundation.NSURLBookmarkCreationWithSecurityScope,
-            None,
-            None,
-            None)
-        if error:
-            # that's the error code if the file is not on disk yet.
-            if error.code() == 260:
-                return None
-            raise Exception(error.localizedDescription())
-        # base64 encode the bookmark so it can be jsonified
-        return bookmark.base64EncodedStringWithOptions_(0)
+    def add_or_replace_attribute(element, new_value):
+        attr = element.attributes.itemByName('nraynaud-Export', 'export')
+        if attr:
+            previous_content = list(json.loads(attr.value))
+            if not next((e for e in previous_content if e['file'] == new_value['file']), None):
+                previous_content.append(new_value)
+                attr.value = json.dumps(previous_content)
+        else:
+            element.attributes.add('nraynaud-Export', 'export', str(json.dumps([new_value])))
 
     def on_execution(args):
-        file_name = file_input.text
-        body = selection_input.selection(0).entity
-        chain = get_path_for_entity(body)
-        the_bytes = get_bookmark_bytes(file_name)
-        need_save = True
-        if not the_bytes:
-            export_stl(design, body, file_name)
+        for row in deletion_table:
+            file_name = row['file']
+            body = row['body']
             the_bytes = get_bookmark_bytes(file_name)
-            need_save = False
-        json_params = json.dumps({'file': the_bytes, 'chain': chain})
-        # for later
-        #        body.attributes.add('nraynaud-Export', 'export', json.dumps([{'file': the_bytes, 'type': 'stl'}]))
-        dir_param = design.userParameters.itemByName(USER_PARAM)
-        if dir_param:
-            dir_param.comment = json_params
-        else:
-            design.userParameters.add(USER_PARAM, ValueInput.createByString('0'), '', json_params)
-        if need_save:
-            ui.commandDefinitions.itemById(EXPORT_COMMAND_KEY).execute()
+            attr = body.attributes.itemByName('nraynaud-Export', 'export')
+            if attr:
+                previous_content = json.loads(attr.value)
+                left_content = [e for e in previous_content if e['file'] != the_bytes]
+                if len(left_content):
+                    attr.value = json.dumps(left_content)
+                else:
+                    attr.deleteMe()
+        for row in shadow_table:
+            file_name = row['file']
+            body = row['body']
+            the_bytes = get_bookmark_bytes(file_name)
+            need_save = True
+            if not the_bytes:
+                export_stl(design, body, file_name)
+                the_bytes = get_bookmark_bytes(file_name)
+                need_save = False
+            add_or_replace_attribute(body, {'file': the_bytes, 'type': 'stl'})
+            if need_save:
+                export_to_bookmark(the_bytes, design, body)
 
+    cmd.inputChanged.add(handle(handle_input_change, InputChangedEventHandler))
+    cmd.activate.add((handle(on_activation, CommandEventHandler)))
     cmd.execute.add(handle(on_execution, CommandEventHandler))
 
 
-def export_stl_file(args):
-    app = Application.get()
-    ui = app.userInterface
-    design = app.activeProduct
-    dir_param = design.userParameters.itemByName(USER_PARAM)
-    if dir_param:
-        data = json.loads(dir_param.comment)
-        body = find_entity_from_path(data['chain'], design.rootComponent, ui)
-        if body:
-            nsdata = Foundation.NSData.alloc().initWithBase64EncodedString_options_(data['file'], 0)
-            url, is_stale, error = Foundation.NSURL.URLByResolvingBookmarkData_options_relativeToURL_bookmarkDataIsStale_error_(
-                nsdata,
-                Foundation.NSURLBookmarkResolutionWithSecurityScope,
-                None,
-                None,
-                None)
-            file_path = url.path()
-            accessible = url.startAccessingSecurityScopedResource()
-            if accessible:
-                try:
-                    return export_stl(design, body, file_path)
-                finally:
-                    url.stopAccessingSecurityScopedResource()
-            else:
-                ui.messageBox('file ' + str(url) + ' is not accessible')
-        else:
-            ui.messageBox('body "' + '.'.join(data['chain']) + '" not found')
-    ui.commandDefinitions.itemById(SETTINGS_COMMAND_KEY).execute()
+def get_bookmark_bytes(file_name):
+    url = Foundation.NSURL.alloc().initFileURLWithPath_(file_name)
+    bookmark, error = url.bookmarkDataWithOptions_includingResourceValuesForKeys_relativeToURL_error_(
+        Foundation.NSURLBookmarkCreationWithSecurityScope,
+        None,
+        None,
+        None)
+    if error:
+        # that's the error code if the file is not on disk yet.
+        if error.code() == 260:
+            return None
+        raise Exception(error.localizedDescription())
+    # base64 encode the bookmark so it can be jsonified
+    return bookmark.base64EncodedStringWithOptions_(0)
+
+
+def decode_bookmark(bookmark, access_action=None):
+    nsdata = Foundation.NSData.alloc().initWithBase64EncodedString_options_(bookmark, 0)
+    url, is_stale, error = Foundation.NSURL.URLByResolvingBookmarkData_options_relativeToURL_bookmarkDataIsStale_error_(
+        nsdata, Foundation.NSURLBookmarkResolutionWithSecurityScope, None, None, None)
+    if error:
+        raise Exception(error.localizedDescription())
+    file_path = url.path()
+    if access_action:
+        accessible = url.startAccessingSecurityScopedResource()
+        if accessible:
+            try:
+                return access_action(file_path)
+            finally:
+                url.stopAccessingSecurityScopedResource()
+    else:
+        return file_path
+
+
+def export_to_bookmark(bookmark, design, body):
+    return decode_bookmark(bookmark, lambda file_path: export_stl(design, body, file_path))
+
+
+def export_all_files(args):
+    exports = get_export_list()
+    design = Application.get().activeProduct
+    for export in exports:
+        export_to_bookmark(export['file'], design, export['body'])
 
 
 def export_stl(design, body, file_path):
@@ -200,7 +305,6 @@ def run(context):
     app = Application.get()
     ui = app.userInterface
     try:
-        print('STARTING')
         cmd_defs = ui.commandDefinitions
         export_command = ui.commandDefinitions.itemById(EXPORT_COMMAND_KEY)
         if export_command:
@@ -214,7 +318,7 @@ def run(context):
         settings_command = cmd_defs.addButtonDefinition(SETTINGS_COMMAND_KEY, 'Configure STL export',
                                                         'Configure STL export',
                                                         'configureExportIcon')
-        export_command.commandCreated.add(handle(export_stl_file, CommandCreatedEventHandler))
+        export_command.commandCreated.add(handle(export_all_files, CommandCreatedEventHandler))
         settings_command.commandCreated.add(handle(create_setting_panel, CommandCreatedEventHandler))
         deleters.append(
             replace_existing_control(ui.allToolbarPanels.itemById('SolidMakePanel').controls, settings_command))
